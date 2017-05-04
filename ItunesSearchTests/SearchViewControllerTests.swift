0654810293ca0b01e25a7ca05dfc3e380d0bb423
saroar <saroar9@gmail.com>
2017-05-04 22:34:56 +0300

    start test

---
+++ ItunesSearchTests/SearchViewControllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewControllerTests.swift
+//  ItunesSearch
+//
+//  Created by Alif on 04/05/2017.
+//  Copyright © 2017 Alif. All rights reserved.
+//
+
+import XCTest
+@testable import ItunesSearch
+
+class SearchViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+}
